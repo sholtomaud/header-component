da7bb937790e824c3e13b993b84624874fa8055f
Sholto Maud <sholto.maud@gmail.com>
2016-06-18 17:34:29 +1000

    fixup

--- headerComponent.js
+++ headerComponent.js
@@ -12,58 +12,8 @@ module.exports = function (fastn, component, type, settings, children) {
   component.extend('_generic', settings, children)
   component.setProperty('valid')
 
-  // console.log('settings', settings)
-
-  component.setProperty('leftMenu')
-  component.leftMenu(settings.leftMenu !== false)
-
-  component.setProperty('title')
-  component.title(settings.title !== false)
-  // component.title.value = settings.title
-
-  component.setProperty('rightMenu')
-  component.rightMenu(settings.rightMenu !== false)
-
-  component.leftMenu = crel('div', {
-    class: 'leftMenu'
-  }, settings.leftMenu)
-
-  function leftMenu () {
-    if (!settings.leftMenu) {
-
-    }
-
-  }
-
-  const leftIcon = require('svg-icon-component')({
-
-          // Function to resolve the url location of an icon.
-    resolvePath: function (iconName) {
-
-      return 'foo/bar/baz' + iconName + '.svg'
-
-    }
-
-  })
-
-
-
-  component.title = crel('div', {
-    class: 'title'
-  }, settings.title)
-
-  component.rightMenu = crel('div', {
-    class: 'rightMenu'
-  }, settings.rightMenu)
-
   component.render = function () {
-    component.element = crel('div', {class: 'header'},
-      component.leftMenu || '',
-      component.title || '',
-      component.rightMenu || ''
-    )
-    component.emit('render')
-
+    component.element = crel('div', {class: 'header'})
   }
   return component
 }
